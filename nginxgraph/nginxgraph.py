86f4183b472039f4aaf91262e159fa0812b1fb55
sirMackk <c.tairix@gmail.com>
2013-08-04 11:13:04 -0400

    Adds failing tests and some code to parser

--- nginxgraph/nginxgraph.py
+++ nginxgraph/nginxgraph.py
@@ -1,84 +1,117 @@
-#import re, sys
-      # Setup user input and output files
-  # script, input, output = argv
-  #so derty here
+import re
+import pygeoip
 
-  f = open(input, 'rb')
-  lines = f.readlines()
-  f.close()
+class Nginxgraph(object):
+    def __init__(self, file_like_object):
+        self.file = file_like_object
+        try:
+          log = open(self.file, 'rb')
+        except IOError:
+          print "Cannot find input file"
+          exit(1)
 
-  out = open('%s.csv' % output, 'wb')
-  # ip_out = open('%s_ip.csv' % output, 'wb')
-  # path_out = open('%s_path.csv' % output, 'wb')
+        lines = log.readlines()
+        log.close()
 
-  writer = Csv(out)
+        return analyze_log(lines)
 
-  gi = pygeoip.GeoIP('GeoLiteCity.dat')
+    def analyze_log(self, lines):
+        """
+        main log analysis function composed of smaller
+        functions targeting specific parts of the log
+        """
+        data = []
+        for val in lines:
+            row = []
+            row += self.get_geo_ip(val)
+            row += self.get_time(val)
 
-  # ips = PopularItem()
-  # paths = PopularItem()
+            remainder = val[30:]
+            row += self.get_request_type(remainder)
+            row += self.get_request_response(remainder)
+            row += self.get_reply_size(remainder)
 
-  # Main routine
-  # try:
-  for i in lines:
-      chopped = i.split('-')
-      split = i.split('"')
-      ip = chopped[0].strip()
-      geoip = gi.record_by_addr(ip)
-      country = geoip['country_name']
-      country_code = geoip['country_code']
-      city = geoip['city']
+            row += self.get_request_path(remainder)
+            row += self.get_user_agent(i)
+          
+            data.append(row)
 
-      # ips.insert(ip)
+        return data
+
+    def get_geo_ip(self, line):
+        """
+        return a list of ip related geographical location data.
+        """
+
+        try:
+            gi = pygeoip.GeoIP('GeoLiteCity.dat')
+        except IOError:
+            print 'Missing GeoLiteCity.dat file'
+            exit(1)
+
+        ip = line.split('-')[0].strip()
+        geo_record = gi.record_by_addr(ip)
+
+        return [ip, geo_record['country_name'], geo_record['country_code'],
+            geo_record['city']]
+
+    def get_time(self, line):
+        """
+        return the date and time as it appears in the log
+        """
+
+        return re.search(r'\[[a-zA-Z0-9/:]+', line).group()[1:-7]
+
+    def get_request_type(self, line):
+        """
+        return the http verb associated with a request
+        """
+
+        request_type = re.search(r'"[GETPOSUDHAL]{3, 4}', line)
+        if request_type:
+            return [request_type.group()[1:]]
+        else:
+            return [re.search(r'".+?"', line).group()]
+
+    def get_request_response(self, line):
+        """
+        return the http response code
+        """
+
+        request_response = re.search(r'\s{1}[2345][0-9]{2}\s{1}', line)
+        if request_response:
+            return request_response.group().strip()
+        else:
+            return null
+
+    def get_reply_size(self, line):
+        """
+        return the reply size in bytes
+        """
+
+
+        return re.search(r'\d+', re.search(r'\d{1,5} "', bulk_rest).group()).group()
+
+    def get_request_path(self, line):
+        """
+        return the queried path
+        """
+
+        request_path = re.search(r'\s(\/[a-zA-Z0-9-_\/\!\@\#\$\%\^\*\.]+|\/\s)', line)
+        if request_path:
+            return request_path.group().strip()
+        else:
+            return 'No request path?'
+
+    def get_user_agent(self, line):
+        """
+        return the user agent
+        """
         
-      time = re.search(r'\[[a-zA-Z0-9/:]+', i).group()[1:-7]
-      #arbitrary number to make parsing a wee bit easier
-      bulk_rest = i[25:]
-          try:
-          request_type = re.search(r'"[GETPOSUDHAL]{3,4}', bulk_rest).group()[1:]
-      except AttributeError, e:
-          print "Request type error: ", e
-          request_type = re.search(r'".+?"', bulk_rest).group()
-          print request_type
-      try:
-          request_response = re.search(r'\s{1}[2345][0-9]{2}\s{1}', bulk_rest).group().strip()
-      except:
-          request_response = 'error'
-      try:
-          request_path = re.search(r'\s(\/[a-zA-Z0-9-_\/\!\@\#\$\%\^\*\.]+|\/\s)', bulk_rest).group().strip()
-      except AttributeError, e:
-          print 'Error: ', e
-          print bulk_rest
-          request_path = 'No request path?'
-
-      # paths.insert(request_path)
-
-      request_reply_size = re.search(r'\d+', re.search(r'\d{1,5} "', bulk_rest).group()).group()
-      agent = split[-2].replace(',', '.')
-      # write vs insert
-      writer.insert([ip, country, country_code, city,
-                  time, request_type, request_response, request_reply_size,
-                  request_path, agent])
-
-  writer.output_list()
-
-  js = open('countries.js', 'wb')
-  ToJson(js, writer.master_list, 1)
-  js.close()
+        return line.split('"')[-2].replace(',', '.')
 
 
-  writer.close()
-  # writer = Csv(ip_out)
-  # for ip, frequency in ips.master_list.iteritems():
-  #   writer.write([ip, frequency])
-  # writer.close()
-  # writer = Csv(path_out)
-  # for path, frequency in paths.master_list.iteritems():
-  #   writer.write([path, frequency])
-  # writer.close()
-  print 'Created %s.csv' % output
-  # except:
-  #   print 'error'
-  #   out.close()
-  #   exit(1)
-  # out.close()
+
+
+
+
--- tests/nginx_tests.py
+++ tests/nginx_tests.py
@@ -4,47 +4,59 @@ from StringIO import StringIO
 import mock #?
 
 
-class Nginxgraph_tests(self):
+class Nginxgraph_tests(object):
 
     def setUp(self):
         test_file = StringIO()
-        test_file.write('031.513.124.312 - - [12/Jul/2013:15:00:11 +0000]\
+        test_file.write('93.184.216.119 - - [12/Jul/2013:15:00:11 +0000]\
          "CONNECT mattscodecave.com:443 HTTP/1.1" 400 172 "-" "-"\n\
          99.12.124.12 - - [12/Jul/2013:15:00:14 +0000] "\\x04\\x01\\x00P>\\xE\
-         Cl\\xC80\\x00" 400 172 "-" "-"\n10.0.0.1 - - [25/Jun/2013:08:00:21\
+         Cl\\xC80\\x00" 400 172 "-" "-"\n93.184.216.119 - - [25/Jun/2013:08:00:21\
           +0000] "GET /w00tw00t.at HTTP/1.1"\
-           301 184 "-" "bebs"\n123.123.511.123 - - [25/Jun/2013:11:00:41 +0000]\
+           301 184 "-" "bebs"\n93.184.216.119 - - [25/Jun/2013:11:00:41 +0000]\
             "GET /assets/styles-yo.css HTTP/1.1" 200 3623 "-" "UserAgent \n')
 
-        self.test_line = '123.123.511.123 - - [25/Jun/2013:11:00:41 +0000]\
-            "GET /assets/styles-yo.css HTTP/1.1" 200 3623 "-" "UserAgent \n'
+        self.test_line = '93.184.216.119 - - [25/Jun/2013:11:00:41 +0000]\
+            "GET /assets/styles-yo.css HTTP/1.1" 200 3623 "-" "Mozilla/5.0\
+             (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+              Chrome/28.0.1500.72 Safari/537.36\n'
 
-        self.parser = Nginxgraph(test_file)
+        self.nginxgraph = Nginxgraph() #nah, has to be a better wai
 
-    def test_getIP(self):
-        self.parser.getIP
-
-    def test_getGeo(self):
-        pass
+    def test_get_geo_ip(self):
+        geo_info = self.nginxgraph.get_geo_IP(self.test_line)
+        geo_info_parsed = ['93.184.216.119', 'United States', 'US', '']
+        assert_equal(geo_info, geo_info_parsed)
 
     def test_get_time(self):
-        pass
+        date = self.nginxgraph.get_time(self.test_line)
+        assert_equal(date, '25/Jun/2013:11:00:41')
 
     def test_get_request_type(self):
-        pass
+        request_type = self.nginxgraph.get_request_type(self.test_line[25:])
+        assert_equal(request_type, 'GET')
+        #expand to include more intricate requests or lack of request
 
     def test_get_request_response(self):
-        pass
+        request_response = self.nginxgraph.get_request_response(self.test_line[25:])
+        assert_equal(request_response, '200')
 
     def test_get_request_path(self):
-        pass
+        request_path = self.nginxgraph.get_request_path(self.test_line[25:])
+        assert_equal(request_path, '/assets/styles-yo.css')
+        #expand to checkout more path types
 
     def test_get_reply_size(self):
-        pass
+        reply_size = self.nginxgraph.get_reply_size(self.test_line)
+        assert_equal(reply_size, '3623')
 
     def test_get_user_agent(self):
-        pass
+        request_u_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64)\
+         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72\
+          Safari/537.36'
+        u_agent = self.nginxgraph.get_user_agent(self.test_line)
+        assert_equal(u_agent, request_u_agent)
 
-    def test_output_row(self):
-        pass
+    # def test_output_row(self):
+    #     pass
 
